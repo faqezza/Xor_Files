ed5ce82d2362513d8b116646ab82c4918bcb9c31
Faqez <148907985+faqezza@users.noreply.github.com>
2024-02-26 18:17:06 -0300

    Update xor_files.py

--- xor_files.py
+++ xor_files.py
@@ -1,4 +1,11 @@
 from pwn import xor
+from termcolor import colored
+import pyfiglet
+
+def imprimir_banner(texto):
+    banner = pyfiglet.figlet_format(texto, font="smslant")
+    texto_verde = colored(banner, color='green')
+    print(texto_verde)
 
 def encrypt_file(filename, key):
     with open(filename, "rb") as file:
@@ -14,19 +21,20 @@ def decrypt_file(filename, key):
     with open(filename[:-10], "wb") as file:  # Remove a extensão ".encrypted" do nome do arquivo
         file.write(decrypted_data)
 
-mode = input("Deseja criptografar (c) ou descriptografar (d) um arquivo? ")
+if __name__ == "__main__":
+    imprimir_banner(texto='XOR       FILES')
+    mode = input("Deseja criptografar (c) ou descriptografar (d) um arquivo? ")
 
-if mode.lower() == "c":
-    filename = input("Digite o nome do arquivo a ser criptografado: ")
-    password = input("Digite sua senha: ").encode()
-    encrypt_file(filename, password)
-    print("Arquivo criptografado com sucesso!")
-elif mode.lower() == "d":
-    filename = input("Digite o nome do arquivo a ser descriptografado: ")
-    password = input("Digite sua senha: ").encode()
+    if mode.lower() == "c":
+        filename = input("Digite o nome do arquivo a ser criptografado: ")
+        password = input("Digite sua senha pessoal: ").encode()
+        encrypt_file(filename, password)
+        print("Arquivo criptografado com sucesso!")
 
-    # Descriptografar o arquivo
-    decrypt_file(filename, password)
-    print("Arquivo descriptografado com sucesso!")
-else:
-    print("Opção inválida. Escolha 'c' para criptografar ou 'd' para descriptografar.")
+    elif mode.lower() == "d":
+        filename = input("Digite o nome do arquivo a ser descriptografado: ")
+        password = input("Digite sua senha pessoal: ").encode()
+        decrypt_file(filename, password)
+        print("Arquivo descriptografado com sucesso!")
+    else:
+        print("Opção inválida. Escolha 'c' para criptografar ou 'd' para descriptografar.")
